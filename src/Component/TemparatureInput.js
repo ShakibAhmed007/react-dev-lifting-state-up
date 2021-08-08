c23ebe8b872295213f765a651c94761007bd9849
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-08 17:21:57 +0600

    1 file modified

---
+++ src/Component/TemparatureInput.js
@@ -0,0 +1,30 @@
+import BoilingVerdict from './BoilingVerdict';
+import React from 'react';
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = e => {
+    this.props.handleChange(e.target.value);
+  };
+
+  render() {
+    const temperature = this.props.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[this.props.scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;
